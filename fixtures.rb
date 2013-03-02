8a3ca71c84c7d8949632781cc0c4806479cbae1a
Irina Dumitrascu <me@dira.ro>
2013-03-02 21:50:52 +0100

    More books

--- fixtures.rb
+++ fixtures.rb
@@ -1,2 +1,6 @@
 # The complete Hitchhikers's guide to the Universe
 Book.create({soundcloud_id: 50944382, readmill_id: 19008})
+# Lord of the flies
+Book.create({soundcloud_id: 24231070, readmill_id: 9881})
+# Murder is easy
+Book.create({soundcloud_id: 73347610, readmill_id: 22340})
